3a80a9cef09c6d009a31ac4bf0713f4d38e2416d
Luciano Correia <luciano.correia@gmail.com>
2022-11-16 14:49:06 +0400

    handle db errors

--- util/random.go
+++ util/random.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"math/rand"
 	"strings"
 	"time"
@@ -45,4 +46,9 @@ func RandomCurrency() string {
 	currencies := []string{"EUR", "USD", "BRL"}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
+}
+
+// RandomEmail returns a random email
+func RandomEmail() string {
+	return fmt.Sprintf("%s@email.com", RandomString(6))
 }
