f275a2d952152815571da60fffd98208d5df3b97
Luciano Correia <luciano.correia@gmail.com>
2022-11-17 16:53:03 +0400

    add jwt maker

---
+++ token/payload.go
@@ -0,0 +1,47 @@
+package token
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var (
+	ErrInvalidToken = errors.New("token is invalid")
+	ErrExpiredToken = errors.New("token has expired")
+)
+
+// Payload contains the payload data for the token
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiresAt time.Time `json:"expires_at"`
+}
+
+// NewPayload returns a new instance of the payload
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		ID:        tokenID,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiresAt: time.Now().Add(duration),
+	}
+
+	return payload, nil
+}
+
+// Valid returns true if the payload is valid
+func (p *Payload) Valid() error {
+	if time.Now().After(p.ExpiresAt) {
+		return ErrExpiredToken
+	}
+
+	return nil
+}
